e862e960585702f51eb3f656d54ae9d5e93b8a73
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-09-20 18:11:57 -0700

    ready for check

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const shoppingCart = getLocalStorage("cart");
+  shoppingCart.push(product);
+
+  setLocalStorage("cart", shoppingCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
