8b0d7614b6a3b0709120890a42068a07e1d67ac2
gyanqui <gyanqui3@gmail.com>
2024-09-25 22:57:35 -0500

    Fixing errors assignment 2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,16 +1,17 @@
 import {renderListWithTemplate} from "./utils.mjs";
 
-function productCartTemplate(product) {
+function productCardTemplate(product) {
     return `<li class="product-card">
-    <a href="product_page/index.html?product=">
-        <img src="" alt="Image of ">
-        <h3 class="card__brand"></h3>
-        <h2 class="card__name"></h2>
-        <p class="product-card__price">$</p>
-        </a>
-    </li>`
-}
-
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img
+        src="${product.Image}"
+        alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.Name}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+</li>`;
+}   
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
