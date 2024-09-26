338f91fd5309498d4ccb40db1a8ebfe890d8daac
gyanqui <gyanqui3@gmail.com>
2024-09-25 19:15:16 -0500

    Completing individual assignment 2

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -26,3 +26,19 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
