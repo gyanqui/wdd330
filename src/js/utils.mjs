914816ebad0dbf59caab8052a35a9ff10ab6c7b0
aster <asterparedes0912@gmail.com>
2024-09-27 17:39:46 -0700

    individual stash

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -26,3 +26,13 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlStrings = list.map(templateFn)
+
+  parentElement.insertAdjacentHTML(position,htmlStrings.join(""));
+}
