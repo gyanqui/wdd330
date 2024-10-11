27615da0d12165ba91b56708d4ca0cc5d93cdb80
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-10-11 07:48:50 -0700

    my task done call to action

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+displayPopup();
+
+
+let popup = document.getElementById("popup");
+
+function setLastVist() {
+    window.localStorage.setItem("lastVisit", JSON.stringify(new Date()));
+}
+
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup")
+}
+
+function displayPopup() {
+    if (!lastVisit) {
+        openPopup();
+    }
+    setLastVist();
+}
+
