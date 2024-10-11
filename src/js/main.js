3c58405d284551cce22fa11f91647c1adb00941c
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-10-11 09:13:08 -0700

    call to action

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-displayPopup();
 
 
-let popup = document.getElementById("popup");
+var button = document.getElementById("close");
+button.addEventListener("click", closePopup);
+
+
 
 function setLastVist() {
     window.localStorage.setItem("lastVisit", JSON.stringify(new Date()));
@@ -12,17 +14,26 @@ function setLastVist() {
 
 
 function openPopup() {
+    let popup = document.getElementById("popup");
     popup.classList.add("open-popup");
 }
 
 function closePopup() {
-    popup.classList.remove("open-popup")
+
+    let popup = document.getElementById("popup");
+    popup.classList.remove("open-popup");
+
 }
 
 function displayPopup() {
+
+    let lastVisit = Date.parse(JSON.parse(window.localStorage.getItem("lastVisit")));
+
+
     if (!lastVisit) {
         openPopup();
     }
     setLastVist();
 }
 
+displayPopup();
