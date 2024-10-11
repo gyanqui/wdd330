a99b5ec4bd12c8197abeac07410c3de5e79b7f88
Jacob-Canyon <145243883+Jacob-Canyon@users.noreply.github.com>
2024-10-10 23:06:02 -0700

    individual activity 4

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
@@ -94,6 +94,8 @@ export default class CheckoutProcess {
     try {
       const res = await services.checkout(json);
       console.log(res);
+      setLocalStorage("cart", []);
+      location.assign("/checkout/success.html")
     } catch (err) {
       console.log(err);
     }
