c1db7b2652f955d6f855d2839d8325360e16a919
aster <asterparedes0912@gmail.com>
2024-10-11 22:57:00 -0700

    Newsletter sign up
    Please check my code.

--- src/js/main.js
+++ src/js/main.js
@@ -37,3 +37,29 @@ function displayPopup() {
 }
 
 displayPopup();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.querySelector("#email");
+    const message = document.querySelector("#message");
+
+    document.querySelector("#subscribeNewsletter").addEventListener("click", (e) => {
+        e.preventDefault();
+        const email = emailInput.value.trim();
+
+        if (validEmail(email)) {
+            message.textContent = "Thank you for subscribing!";
+            message.classList.remove("hidden");
+            emailInput.value = "";
+            message.style.color = "#525b0f";
+        } else {
+            message.textContent = "Please enter a valid email address."
+            message.classList.remove("hidden");
+            message.style.color = "red";
+        }
+      });
+
+      function validEmail(email) {
+        const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        return emailRegex.test(email);
+      }
+})
